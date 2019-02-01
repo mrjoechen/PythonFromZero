225a64a4eaefbc06739a4ac6e24541cfa60f69f3
chenqiao <mrjoechen@qq.com>
2019-02-01 17:40:44 +0800

    Initial commit

---
+++ init/07/network_bandwidth.py
@@ -0,0 +1,6 @@
+# 网络带宽计算
+
+bandwidth = 8
+ratio = 100
+
+print(ratio / bandwidth)
---
+++ init/07/zodiac.py
@@ -0,0 +1,35 @@
+zodiac_name = (u'摩羯座', u'水瓶座', u'双鱼座', u'白羊座', u'金牛座', u'双子座',
+           u'巨蟹座', u'狮子座', u'处女座', u'天秤座', u'天蝎座', u'射手座')
+
+#u表示unicode
+zodiac_days = ((1, 20), (2, 19), (3, 21), (4, 21), (5, 21), (6, 22),
+              (7, 23), (8, 23), (9, 23), (10, 23), (11, 23), (12, 23))
+
+
+(month, day) = (2, 18)
+
+zodiac_day = filter(lambda  x: x<=(month, day), zodiac_days)
+# print(zodiac_day)
+
+zodac_len = len(list(zodiac_day)) % 12
+print(zodiac_name[zodac_len])
+
+a_list = ['abc', 'xyz']
+a_list.append('X')
+print (a_list)
+a_list.remove('xyz')
+print(a_list)
+
+# 鼠牛虎兔龙蛇马羊猴鸡狗猪
+
+animals = ('鼠牛虎兔龙蛇马羊猴鸡狗猪')
+
+for animal in animals:
+    print(animal)
+
+
+for i in range(2, 11):
+    print(i)
+
+for i in range(len(animals)):
+    print(i+1, animals[i])
